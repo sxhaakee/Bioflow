3fbce76cd9f82a632d95f617f71577a970e6bd60
MOHAMMED SHAKEEB <149256691+sxhaakee@users.noreply.github.com>
2024-02-20 19:02:41 +0530

    Delete codeblaze directory

--- codeblaze/script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Delay the animation to start after a short delay (adjust the delay as needed)
-    setTimeout(function() {
-        document.querySelector("tittle").style.opacity = "1";
-        document.querySelector("tittle").style.transform = "translateY(0)";
-        
-    }, 500); // 500ms delay
-});
